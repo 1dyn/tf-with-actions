c83a4e206371adf9ae82cc4519724027d0b9007d
1dyn <gksehddus202@naver.com>
2025-06-25 11:43:31 +0900

    add modules

---
+++ modules/dynamodb/outputs.tf
@@ -0,0 +1,8 @@
+output "dynamodb_table_name" {
+  description = "DynamoDB table name"
+  value = aws_dynamodb_table.terraform-backend-lock.name
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.bucket
+}
