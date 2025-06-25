24af60db5b20e942ce89aed9787e46404900882c
1dyn <gksehddus202@naver.com>
2025-06-25 15:30:32 +0900

    fix

--- provider.tf
+++ provider.tf
@@ -5,14 +5,18 @@ terraform {
       version = ">= 5.0.0"
     }
   }
-  backend "s3" {
-    bucket         = "terraform-state-bucket-dy"
-    key            = "terraform/state-test/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock"
-  }
-}
 
-provider "aws" {
-  region = "ap-northeast-2"
+  cloud {
+    organization = "tfc-devops-tmp"
+    workspaces {
+      name = "tfc-workspace"
+    }
+  }
+
+  # backend "s3" {
+  #   bucket         = "terraform-state-bucket-dy"
+  #   key            = "terraform/state-test/terraform.tfstate"
+  #   region         = "ap-northeast-2"
+  #   dynamodb_table = "terraform-lock"
+  # }
 }
