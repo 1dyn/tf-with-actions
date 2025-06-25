b617cef614353bb4cbab1605d172a48e126aabe8
1dyn <gksehddus202@naver.com>
2025-06-25 13:12:11 +0900

    fix *.tf

--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -1,6 +1,6 @@
 output "dynamodb_table_name" {
   description = "DynamoDB table name"
-  value = aws_dynamodb_table.terraform-backend-lock.name
+  value       = aws_dynamodb_table.terraform-backend-lock.name
 }
 
 output "s3_bucket_name" {
